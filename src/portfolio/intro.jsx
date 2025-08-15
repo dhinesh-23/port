36438e14a3fc25d515ea3b136c71801b2165b9b1
dhinesh-23 <sankarsnr123@gmail.com>
2025-08-15 13:03:08 +0530

    acc update v.02

--- src/portfolio/intro.jsx
+++ src/portfolio/intro.jsx
@@ -9,7 +9,7 @@ export default function Intro(){
          <h1 className="title">Hello! I am web</h1>
          <h1 className="title">Developer</h1>
          <p className="intro-sec-p"> I am a passionate Web Developer with experience in building and designing dynamic websites, mobile applications, and desktop software. I specialize in modern web technologies like React, JavaScript, HTML, and CSS. Let's bring your ideas to life!</p>
-         <a href="./final cv.pdf"><button className='but-3'><span>learn more!</span></button></a>
+         <a href="./final cv.pdf"><button className='but-3'><span>Learn more!</span></button></a>
          <a href="#footer" ><button className='but-2'><span>Hire me</span></button></a>
     </div>
     
