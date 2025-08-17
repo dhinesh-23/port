1e20e9220374896b243db3ca3650b8abce3f1078
dhinesh-23 <sankarsnr123@gmail.com>
2025-08-17 13:58:39 +0530

    update project v.04

--- src/boys/intro.jsx
+++ src/boys/intro.jsx
@@ -7,8 +7,8 @@
             <img src='./into.png' alt="tshits" className="into"/>
             </div>
            <div className="content">
-            <h1>Stay soft, fight hard..<i class="fa fa-bullhorn"></i></h1>
-             <h1>No limits, just goals..<i class="fa fa-check-square"></i></h1>
+            <h1>Stay soft, fight hard..📢</h1>
+             <h1>No limits, just goals..<span className="i">✔</span></h1>
               <button className="btn-into">shop now</button>
            </div>
         </div>
